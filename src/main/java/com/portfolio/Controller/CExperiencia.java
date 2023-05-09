172c8d13524805339095f12aaeb028656166122b
facurobles <hfacu.robles@gmail.com>
2023-05-09 01:04:26 -0300

    ..

--- src/main/java/com/portfolio/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/Controller/CExperiencia.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = {"http://localhost:4200", "https://portfolio-front-robles.web.app"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://portfolio-front-robles.web.app/"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
